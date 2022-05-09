5630d78fcde3a7e55588f9a47cce6676456ba6c6
Anthony <russo555@gmail.com>
2022-05-09 13:15:52 -0400

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,80 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.66.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = var.default_tags
+  }
+}
+
+module "primary_kubernetes" {
+  source                       = "./k8s"
+  eks_cluster_name             = var.eks_cluster_name_primary
+  eks_vpc_cidr_block           = var.eks_cluster_primary_ips
+  eks_vpc_cidr_block_primary   = var.eks_cluster_primary_ips
+  eks_vpc_cidr_block_secondary = var.eks_cluster_secondary_ips
+  eks_primary_cluster          = var.eks_cluster_name_primary
+  availability_zones           = var.availability_zones
+
+}
+
+module "secondary_kubernetes" {
+  source                       = "./k8s"
+  eks_cluster_name             = var.eks_cluster_name_secondary
+  eks_vpc_cidr_block           = var.eks_cluster_secondary_ips
+  eks_vpc_cidr_block_primary   = var.eks_cluster_primary_ips
+  eks_vpc_cidr_block_secondary = var.eks_cluster_secondary_ips
+  eks_primary_cluster          = var.eks_cluster_name_secondary
+  availability_zones           = var.availability_zones
+}
+
+module "create_peering_secondary_to_primary" {
+  source           = "./peering"
+  vpc_id_accepter  = module.primary_kubernetes.vpc_id
+  vpc_id_requester = module.secondary_kubernetes.vpc_id
+}
+
+# Sets up routing for VPC Peering. Runs after VPCs have been created.
+# Creates the route for the peering connection from secondary's perspective
+module "post_vpc_creation_routing_rules_secondary_to_primary" {
+  source                    = "./post_routing"
+  vpc_peering_connection_id = module.create_peering_secondary_to_primary.vpc_peering_connection_id
+  routing_table_id          = module.secondary_kubernetes.route_table_id
+  vpc_id                    = module.secondary_kubernetes.vpc_id
+  cidr_block_transit        = var.eks_cluster_primary_ips.vpc
+  main_route_table          = module.secondary_kubernetes.main_route_table_id
+  private_route_table       = module.secondary_kubernetes.route_table_id
+}
+
+# Sets up routing for VPC Peering. Runs after VPCs have been created.
+# Creates the route for the peering connection from secondary's perspective
+module "post_vpc_creation_routing_rules_primary_to_secondary" {
+  source                    = "./post_routing"
+  vpc_peering_connection_id = module.create_peering_secondary_to_primary.vpc_peering_connection_id
+  routing_table_id          = module.primary_kubernetes.route_table_id
+  vpc_id                    = module.primary_kubernetes.vpc_id
+  cidr_block_transit        = var.eks_cluster_secondary_ips.vpc
+  main_route_table          = module.primary_kubernetes.main_route_table_id
+  private_route_table       = module.primary_kubernetes.route_table_id
+}
+
+
+module "cleanup_infra_primary" {
+    source = "github.com/webdog/terraform-kubernetes-delete-eni"
+    vpc_id = module.primary_kubernetes.vpc_id
+    region = var.aws_region
+}
+
+
+module "cleanup_infra_secondary" {
+    source = "github.com/webdog/terraform-kubernetes-delete-eni"
+    vpc_id = module.secondary_kubernetes.vpc_id
+    region = var.aws_region
+}
---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "eks_cluster_name_primary" {
+  description = "Name of the primary EKS Cluster"
+  type        = string
+  default     = "wp-odr-primary"
+}
+variable "eks_cluster_name_secondary" {
+  type        = string
+  description = "Name of the secondary EKS Cluster"
+  default     = "wp-odr-secondary"
+}
+
+# Default tags to pass to AWS. You can set them here, or in a tfvars file.
+variable "default_tags" {
+  type = map(string)
+  default = {
+    github = "hashicorp/learn-waypoint-odr"
+  }
+}
+
+variable "eks_cluster_primary_ips" {
+  description = "The CIDR groups for the Primary Cluster's IP addresses"
+  default = {
+    vpc      = "10.100.0.0/16"
+    private  = "10.100.1.0/24"
+    public   = "10.100.2.0/24"
+    internet = "0.0.0.0/0"
+  }
+}
+
+variable "eks_cluster_secondary_ips" {
+  description = "The CIDR groups for the Secondary Cluster's IP addresses"
+  default = {
+    vpc      = "172.30.0.0/16"
+    private  = "172.30.1.0/24"
+    public   = "172.30.2.0/24"
+    internet = "0.0.0.0/0"
+  }
+}
+
+variable "aws_region" {
+  default = "ca-central-1"
+  type    = string
+}
+
+variable "availability_zones" {
+  description = "The AZs in which the Clusters will deploy"
+  default = {
+    zone_one = "ca-central-1a"
+    zone_two = "ca-central-1b"
+  }
+}
